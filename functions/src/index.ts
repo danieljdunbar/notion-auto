8101f8434a47219b4d2c33d373bd08f6ec0890fa
Daniel Dunbar <dunbardanielj@gmail.com>
2022-01-11 20:33:54 -0600

    Create daily goals database with api

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,20 +1,154 @@
 import * as functions from 'firebase-functions';
+import { Client } from '@notionhq/client';
+import * as dotenv from 'dotenv';
+import fetch from 'node-fetch';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const notion = new Client({ auth: functions.config().notionapi.key });
+dotenv.config();
 
-exports.notionDailyChanges = functions.pubsub
-  .schedule('every 5 minutes')
-  .onRun((context) => {
-    // Add new daily goals
+// TODO: Figure out how to put new entry at the top of the page
+export const notionDailyGoalsOnce = functions.https.onRequest(
+  async (req, res) => {
+    // get definition for the database to be created
+    const newDailyGoalsTable = generateNewDailyGoalsSchema(
+      functions.config().notionapi.dailygoalspage
+    );
 
-    // Change the home table to reference the new daily goals
+    const newTableInfo = await fetch('https://api.notion.com/v1/databases', {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer ' + functions.config().notionapi.key,
+        'Content-Type': 'application/json',
+        'Notion-Version': '2021-08-16',
+      },
+      body: JSON.stringify(newDailyGoalsTable),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        functions.logger.info('DAILY GOALS TABLE', json);
+        return json;
+      });
 
-    console.log('This will be run every 1 minutes!');
-    return null;
-  });
+    await addDailyGoalsToTable(
+      (newTableInfo as any).id,
+      (newTableInfo as any).properties
+    );
+
+    res.send('Daily goals table created!');
+  }
+);
+
+async function addDailyGoalsToTable(id: string, properties: any) {
+  const goals = [
+    'Keeps',
+    'Dishes',
+    'Spanish',
+    'Journal',
+    'Song writing',
+    'AI',
+    'Meditation',
+    'Stretch',
+    'Sing',
+    'Program',
+    'Workout',
+  ];
+
+  for (const goal of goals) {
+    await notion.pages.create({
+      parent: {
+        database_id: id,
+      },
+      properties: {
+        Name: {
+          type: 'title',
+          title: [
+            {
+              type: 'text',
+              text: {
+                content: goal,
+              },
+            },
+          ],
+        },
+        Status: {
+          type: 'select',
+          select: properties.Status.select.options[0],
+        },
+      },
+    });
+  }
+}
+
+function generateNewDailyGoalsSchema(pageId: string) {
+  return {
+    parent: {
+      type: 'page_id',
+      page_id: pageId,
+    },
+    title: [
+      {
+        type: 'text',
+        text: {
+          content: '@' + getCurrentDate(),
+          link: null,
+        },
+      },
+    ],
+    properties: {
+      Name: {
+        title: {},
+      },
+      Status: {
+        select: {
+          options: [
+            {
+              name: 'Not started',
+              color: 'red',
+            },
+            {
+              name: 'In progress',
+              color: 'yellow',
+            },
+            {
+              name: 'Completed',
+              color: 'green',
+            },
+          ],
+        },
+      },
+      Completed: {
+        formula: { expression: 'prop("Status") == "Completed"' },
+      },
+    },
+  };
+}
+
+function getCurrentDate() {
+  const date_ob = new Date();
+  const date = ('0' + date_ob.getDate()).slice(-2);
+  const month = ('0' + (date_ob.getMonth() + 1)).slice(-2);
+  const year = date_ob.getFullYear();
+  return month + '/' + date + '/' + year;
+}
+
+// exports.notionDailyChanges = functions.pubsub
+//   .schedule('every 5 minutes')
+//   .onRun(async (context) => {
+//     // Add new daily goals table
+//     const response = await fetch('https://api.notion.com/v1/databases', {
+//       method: 'POST',
+//       headers: {
+//         Authorization: 'Bearer ' + process.env.NOTION_INTEGRATION_TOKEN,
+//         'Content-Type': 'application/json',
+//       },
+//     });
+
+//     // Add daily goal entries to new table
+
+//     // Change the home table to reference the new daily goals
+
+//     console.log('This will be run every 1 minutes!');
+//     return null;
+//   });
